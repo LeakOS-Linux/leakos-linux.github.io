8b4cf514930eaf6fb588b1cad594c621f63b04b4
LeakOS-Linux <leakosid7@gmail.com>
2026-02-06 09:24:21 +0700

    first commit

---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/LeakOS-Linux/leakos-linux.github.io.git
+git push -u origin main
